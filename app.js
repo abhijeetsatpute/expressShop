803ff4fac6bfb1d2af430eb111e170a79c78d5b1
Abhijeet Satpute <abhi111sat@gmail.com>
2022-07-11 22:58:56 +0530

    Adding routes in middleware

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((res, req, next) => {
-    console.log('In the middleware !');
-    next(); //Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/add',(req,res,next) => {
+    res.send(
+        `<form action="/product" method="post">
+            <input type='text' name="title"></input>
+            <button type="submit">add</button>
+        </form>`
+    );
 })
 
-app.use((req, res, next) => {
-    console.log('In another middleware !');
-    res.send('<h1>Hello from the Express</h1>');
+app.use('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
 })
 
-//app.listen(3000) --> http.createServer(this) && server.listen(3000)
-app.listen(3000);
+app.use('/',(req,res,next) => {
+    res.send('<h1>Home middleware</h1>')
+})
+
+app.listen(3000)
