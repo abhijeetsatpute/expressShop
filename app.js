15c3de8ebc3b008a08952a8aee144a7f5c7e8742
Abhijeet Satpute <abhi111sat@gmail.com>
2022-07-11 23:21:03 +0530

    Routes seperation

--- app.js
+++ app.js
@@ -3,27 +3,15 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+
 // configure the app to use bodyParser()
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use('/add',(req,res,next) => {
-    res.send(
-        `<form action="/product" method="post">
-            <input type='text' name="title"></input>
-            <button type="submit">add</button>
-        </form>`
-    );
-})
-
-app.post('/product',(req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req,res,next) => {
-    res.send('<h1>Home middleware</h1>')
-})
+app.use(adminRoutes);
+app.use(shopRoute);
 
 app.listen(3000)
