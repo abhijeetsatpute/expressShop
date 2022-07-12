b459e32e7b5656592e4f97e20ada96dea941c674
Abhijeet Satpute <abhi111sat@gmail.com>
2022-07-12 18:27:22 +0530

    Added pug templates
    + installed pug, ejs, express-handlebars
    + converting HTML files to pug files,
    + Setting up views engine to view in app.js:8
    + Specify views path explicitly in app.js:9
    + .render() method -> views & data-props
    + pug syntax

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 
@@ -19,7 +22,8 @@ app.use('/admin', adminData.routes);
 app.use(shopRoute);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.status(404).render('404', {title : "Page not found"});
 });
 
 app.listen(3002)
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,8 @@ const router = express.Router();
 
 router.get('/',(req,res,next) => {
     console.log(adminData.products);
-    res.sendFile(path.join(rootDir, 'views','shop.html'));
+    // res.sendFile(path.join(rootDir, 'views','shop.html'));
+    res.render('shop', { products : adminData.products, title : "My Shop"});
 })
 
 module.exports = router;
