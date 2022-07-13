0bd6e14cba3982ac31a46b431a78822eab11ce95
Abhijeet Satpute <abhi111sat@gmail.com>
2022-07-13 07:55:46 +0530

    Using layouts in handlebars
    + app.js:9
    + views\layouts\main-layout.hbs

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,14 @@ const router = express.Router();
 router.get('/',(req,res,next) => {
     const products = adminData.products;
     // res.sendFile(path.join(rootDir, 'views','shop.html'));
-    res.render('shop', { products : adminData.products, title : "My Shop", path : req.originalUrl, hasProducts : products.length > 0});
+    res.render('shop', { 
+        products : adminData.products, 
+        title : "My Shop", 
+        path : req.originalUrl, 
+        hasProducts : products.length > 0,
+        activeShop: true,
+        productCSS: true
+     });
 })
 
 module.exports = router;
