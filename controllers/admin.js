168a3f7dba167700752daee7b3a5f70fa4451b2c
Abhijeet Satpute <abhi111sat@gmail.com>
2022-07-17 03:38:46 +0530

    Edit Product
    + Using Query params
    + Pre populating the edit product page with data
    + Linking the eidt page with product ids

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,22 +1,39 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req,res,next) => {
-    res.render(
-        'admin/add-product', 
-        {
-            title: 'Add Product', 
-            path : req.originalUrl, 
-            formCSS: true, 
-            productCSS: true, 
-            activeProduct: true
-        }
-    );
+    res.render('admin/edit-product', {
+        title: 'Add Product', 
+        path : req.originalUrl, 
+        editing : false,
+    });
 }
 
 exports.postProducts = (req,res,next) => {
     Product.add(req.body);
     console.log(req.body);
     res.redirect('/');
+}
+
+exports.getEditProduct = (req,res,next) => {
+    const editMode = req.query.edit;
+    
+    if( !(editMode==='true') ) {
+        return res.redirect('/');
+    }
+
+    const productId = req.params.productId;
+    Product.findById(productId, product => {
+        if(!product){
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            title: 'Update Product', 
+            path : req.originalUrl, 
+            editing : true,
+            product : product,
+        });
+    })
+    
 }
 
 exports.getProducts = (req,res,next) => {
