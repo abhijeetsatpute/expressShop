4259623d57e71f2400abe5bc5568c9ee3d7927d5
Abhijeet Satpute <abhi111sat@gmail.com>
2022-07-18 18:18:46 +0530

    Creating & Fetching Cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -41,22 +41,19 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-    Cart.getCart(cart =>{
-        Product.get(products => {
-            const cartProducts = [];
-            for( let product of products) {
-                const cartProductData = cart.products.find(prod => prod.id === product.id);
-                if(cartProductData){
-                    cartProducts.push({productData: product, qty: cartProductData.qty});
-                }
-            }
-            res.render('shop/cart', {
-                path : req.originalUrl,
-                title : "Your Cart", 
-                products : cartProducts,
-            });
-        })
-    });
+    req.user.getCart()
+    .then(cart => {
+        return cart.getProducts()
+            .then(products => {
+                res.render('shop/cart', {
+                    path : req.originalUrl,
+                    title : "Your Cart", 
+                    products : products,
+                });
+            })
+            .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
 }
 
 exports.postCart = (req, res, next) => {
