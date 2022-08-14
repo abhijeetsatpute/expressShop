3f3b3386b0061d2662af5750afdf0dfce441b68e
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-14 18:09:17 +0530

    DB connection pool using sequelize

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,21 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node',
-    password: 'Rudra@911',
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node',
+//     password: 'Rudra@911',
+// });
+
+// module.exports = pool.promise();
+
+//--sequelize: Uses mysql2 behind the scenes to create the connection pool--
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+//dialect for different syntax versions of mysql
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
