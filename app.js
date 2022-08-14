bac7e54ea536571bc3a7c830c6b0d95545647843
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-14 20:30:19 +0530

    Creating and managing a dummy user and passing it to middleware

--- app.js
+++ app.js
@@ -19,6 +19,17 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//using a middleware to pass the user from table
+app.use((req, res, next) => {
+  User.findByPk(1)
+  .then(user => {
+    //storing the user sequelize object in the request itself
+    req.user = user;
+    next();
+  })
+  .catch(err => console.log(err))
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -28,9 +39,20 @@ Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true })
+  // .sync({ force: true })
+  .sync()
   .then(result => {
     // console.log(result);
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if(!user){
+      return User.create({ name: 'John', email: 'john@example.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch(err => {
