b482ebb2e9eb76b0f7876a2e5e007b699ec4cda7
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-15 06:26:46 +0530

    187. Fetching single product: MongoDB stores "_id" as a ObjectId(ID)

--- controllers/shop.js
+++ controllers/shop.js
@@ -23,7 +23,7 @@ exports.getProduct = (req, res, next) => {
   //     });
   //   })
   //   .catch(err => console.log(err));
-  Product.findByPk(prodId)
+  Product.findById(prodId)
   .then((product) => {
     console.log(product.title);
     res.render('shop/product-detail', {
