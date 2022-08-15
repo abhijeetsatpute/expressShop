041cdeba76492bb98c4b23b49477f4bf68ed4b3f
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-15 06:40:40 +0530

    188. Making Edit and Delete product button working by "_id"

--- controllers/admin.js
+++ controllers/admin.js
@@ -28,31 +28,28 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 
-// exports.getEditProduct = (req, res, next) => {
-//   const editMode = req.query.edit;
-//   if (!editMode) {
-//     return res.redirect('/');
-//   }
-//   const prodId = req.params.productId;
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
 
-//   //only editing the products of that specific user
-//   req.user
-//   .getProducts({ where: { id: prodId } })
-//   // Product.findByPk(prodId)
-//   .then(product => {
-//     if(!product){
-//       return res.redirect('/');
-//     }
-//     res.render('admin/edit-product', {
-//       pageTitle: 'Edit Product',
-//       path: '/admin/edit-product',
-//       editing: editMode,
-//       product: product
-//     });
-//   })
-//   .catch(err => { console.log(err)});
+  Product.findById(prodId)
+  .then(product => {
+    if(!product){
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  })
+  .catch(err => { console.log(err)});
   
-// };
+};
 
 // exports.postEditProduct = (req, res, next) => {
 //   const prodId = req.body.productId;
@@ -75,21 +72,19 @@ exports.postAddProduct = (req, res, next) => {
 //     .catch(err => console.log(err));
 // };
 
-// exports.getProducts = (req, res, next) => {
+exports.getProducts = (req, res, next) => {
 
-//   //Getting products of that specific user
-//   req.user
-//   .getProducts()
-//   .then((products) => {
-//     res.render('admin/products', {
-//       prods: products,
-//       pageTitle: 'Admin Products',
-//       path: '/admin/products'
-//     });
-//   })
-//   .catch(err => { console.log(err)});
+  Product.fetchAll()
+  .then((products) => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  })
+  .catch(err => { console.log(err)});
   
-// };
+};
 
 // exports.postDeleteProduct = (req, res, next) => {
 //   const prodId = req.body.productId;
