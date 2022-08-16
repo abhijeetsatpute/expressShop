0f6d337c69186e8ac966cd71dd9015878bb26da8
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-16 18:51:14 +0530

    235. Setting a Cookie with ader('Set-Cookie', 'loggedIn=true') & Getting a cookie with req.get('Cookie')

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,18 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req
+    .get('Cookie')
+    .split(';')[0]
+    .trim()
+    .split('=')[1];
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn
   });
 };
 
 exports.postLogin = (req, res, next) => {
   // Totally Different Request and this gets end
-  req.isLoggedIn = true;
+  res.setHeader('Set-Cookie', 'loggedIn=true');
   res.redirect('/')
 };
