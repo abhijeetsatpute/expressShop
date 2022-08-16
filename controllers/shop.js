74a75673ca777b7d57061e64ef824fd7ef1efa73
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-16 17:15:54 +0530

    222. Loading the cart itemsL execPopulate() to return the promise by populate()

--- controllers/shop.js
+++ controllers/shop.js
@@ -42,15 +42,20 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  req.user.getCart()
-  .then(products => {
-    res.render('shop/cart', {
-      path: '/cart',
-      pageTitle: 'Your Cart',
-      products: products
-    });
-  })
-  .catch(err => console.log(err));
+  req.user
+    .populate('cart.items.productId')
+    // polulate doesnt return a promise we we call execPopulate()
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
+      console.log(products);
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
